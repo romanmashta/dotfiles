eb1745d2fc24a85a4e6c8c718fc5277172744fa4
Roman Mashta <roman.mashta@gmail.com>
2017-11-21 01:06:13 +0200

    Bootstrap shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
+#sh -c "$(curl --no-sessionid -fsSL https://raw.githubusercontent.com/romanmashta/dotfiles/master/bootstrap.sh)"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew cask install iterm2
+
+cd;
+mkdir -p ~/Sandbox
+cd ~/Sandbox
+git clone https://github.com/romanmashta/dotfiles.git
+
+cd ~/Sandbox/dotfiles
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+open /Applications/iTerm.app
+# ./brew.sh
---
+++ brew.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+brew install screen
+brew install dockutil
+brew install midnight-commander	
+brew install python
+brew install watchman
+brew install node
+brew install yarn
