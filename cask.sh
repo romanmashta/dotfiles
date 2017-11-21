7b9c2fd3b30c0dedcac57427d5efd5d5d19d713c
Roman Mashta <roman.mashta@gmail.com>
2017-11-21 02:22:31 +0200

    Dock file

--- cask.sh
+++ cask.sh
@@ -3,13 +3,17 @@
 brew cask install iterm2
 brew cask install sublime-text
 brew cask install the-unarchiver
+brew cask install google-chrome
 
-# brew cask install 010-editor
+# brew cask install istat-menus
+# brew cask install 1password
+# brew cask install vlc
+
 # brew cask install slack
 # brew cask install teamviewer
-# brew cask install google-chrome
-# brew cask install sourcetree
-# brew cask install istat-menus
-# brew cask install parallels
 # brew cask install skype
-# brew cask install vlc
+
+# brew cask install sourcetree
+# brew cask install parallels
+# brew cask install docker
+# brew cask install 010-editor
