6712dc8b0de16ae0b7180374d15e353427287c19
Roman Mashta <roman.mashta@gmail.com>
2017-11-21 02:10:33 +0200

    Update Cask List

--- cask.sh
+++ cask.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 brew cask install iterm2
+brew cask install sublime-text
+brew cask install the-unarchiver
+
+# brew cask install 010-editor
+# brew cask install slack
+# brew cask install teamviewer
+# brew cask install google-chrome
+# brew cask install sourcetree
+# brew cask install istat-menus
+# brew cask install parallels
+# brew cask install skype
+# brew cask install vlc
