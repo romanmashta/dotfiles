59401735cae3fea0855b27e6097b76297d38a3ad
Roman Mashta <roman.mashta@gmail.com>
2017-11-21 02:33:36 +0200

    Dock file update

--- dock.sh
+++ dock.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 
+dockutil --remove Siri
+dockutil --remove Safari
+dockutil --remove Contacts
+dockutil --remove Calendar 
+dockutil --remove Notes
+dockutil --remove Reminders
+dockutil --remove Maps
+dockutil --remove Photos
+dockutil --remove Messages
+dockutil --remove FaceTime
+dockutil --remove iTunes
+dockutil --remove iBooks
+dockutil --remove App\ Store
