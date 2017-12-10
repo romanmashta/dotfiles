9ce0ece6e1767a2ed2f13b1264347c3441055e9d
Roman Mashta <roman.mashta@gmail.com>
2017-12-10 18:44:54 +0200

    Enable cask apps

--- cask.sh
+++ cask.sh
@@ -5,15 +5,15 @@ brew cask install sublime-text
 brew cask install the-unarchiver
 brew cask install google-chrome
 
-# brew cask install istat-menus
-# brew cask install 1password
-# brew cask install vlc
+brew cask install istat-menus
+brew cask install 1password
+brew cask install vlc
 
-# brew cask install slack
-# brew cask install teamviewer
-# brew cask install skype
+brew cask install slack
+brew cask install teamviewer
+brew cask install skype
 
-# brew cask install sourcetree
-# brew cask install parallels
-# brew cask install docker
-# brew cask install 010-editor
+brew cask install sourcetree
+brew cask install parallels
+brew cask install docker
+brew cask install 010-editor
